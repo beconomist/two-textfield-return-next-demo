a097bb2bdd45abe4bb7de4184bd882f9b85ea396
beconomist <beconomist@gmail.com>
2016-05-05 20:43:14 +0800

    First commit

--- ui-demo/ViewController.swift
+++ ui-demo/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var accountTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        accountTextField.delegate = self
+        passwordTextField.delegate = self
+        
+        accountTextField.returnKeyType = .Next
+        passwordTextField.returnKeyType = .Default
+        
+        accountTextField.placeholder = "Enter Account"
+        passwordTextField.placeholder = "Enter Password"
+        passwordTextField.secureTextEntry = true
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        switch textField {
+            
+        case accountTextField:
+            passwordTextField.becomeFirstResponder()
+            print("跳到密碼欄")
+            break
+        case passwordTextField:
+            passwordTextField.resignFirstResponder()
+//            self.view.endEditing(true)
+            print("收鍵盤")
+            break
+        default:
+            break
+        }
+
+        return false
+    }
 
 }
 
